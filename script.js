20a78e74ddb8543063dd995f22f3406f978579f9
bcmray_crazy <975875040@qq.com>
2024-07-14 13:01:51 +0800

    动画跟随鼠标X坐标

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const mainAnimElement = document.querySelector('#main_anim');
 
 var anim = bodymovin.loadAnimation({
-    container: mainAnimElement, // Required
+    container: mainAnimElement,
     path: 'lottie/data.json',
-    renderer: 'svg/canvas/html', // Required
-    loop: true, // Optional
-    autoplay: true, // Optional
-    name: 'Hello World', // Name for future reference. Optional.
+    renderer: 'svg/canvas/html',
+    loop: false,
+    autoplay: false,
+    name: 'test',
+});
+
+let frameTotal = 18;
+
+document.addEventListener('mousemove', (ev) => {
+    const clientWidth = document.body.clientWidth;
+    const time = ((ev.clientX - clientWidth / 4) / clientWidth) * 2;
+    if (time < 0 || time > 1) return;
+    anim.goToAndStop(Math.round(frameTotal * time), true);
 });
